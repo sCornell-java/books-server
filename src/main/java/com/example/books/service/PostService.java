3a86efc993c9de4a666873536581dae4ad598bb2
choiyh0617 <6l0mrc00057@gmail.com>
2025-11-24 12:19:46 +0900

    run

--- src/main/java/com/example/books/service/PostService.java
+++ src/main/java/com/example/books/service/PostService.java
@@ -1,7 +1,9 @@
 package com.example.books.service;
 
+import com.example.books.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface PostService {
+public interface PostService extends JpaRepository<Post,Long> {
 }
