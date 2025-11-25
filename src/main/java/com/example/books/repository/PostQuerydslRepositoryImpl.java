6fd699110c408d26b37a49774f9b04ffe0b439f3
choiyh0617 <6l0mrc00057@gmail.com>
2025-11-25 20:51:20 +0900

    fix: repository

--- src/main/java/com/example/books/repository/PostQuerydslRepositoryImpl.java
+++ src/main/java/com/example/books/repository/PostQuerydslRepositoryImpl.java
@@ -2,10 +2,12 @@ package com.example.books.repository;
 
 import com.example.books.entity.Post;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.example.books.entity.QPost.post;
 
@@ -14,12 +16,43 @@ import static com.example.books.entity.QPost.post;
 public class PostQuerydslRepositoryImpl implements PostQuerydslRepository {
 
     private final JPAQueryFactory queryFactory;
+    private final EntityManager em;
 
     @Override
     public List<Post> findAllByAuthor(String author) {
         return queryFactory
                 .selectFrom(post)
-                .where(post.title.like(author))
+                .where(post.author.eq(author))
                 .fetch();
+    }
+
+    @Override
+    public Post save(Post post) {
+        if (post.getId() == null) {
+            em.persist(post);
+            return post;
+        } else {
+            return em.merge(post);
+        }
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return queryFactory.selectFrom(post).fetch();
+    }
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return Optional.ofNullable(em.find(Post.class, id));
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return findById(id).isPresent();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        findById(id).ifPresent(em::remove);
     }
 }
