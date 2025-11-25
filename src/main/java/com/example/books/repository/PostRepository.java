f009596cad341b2ac0b3d56231ab71bca08c0878
choiyh0617 <6l0mrc00057@gmail.com>
2025-11-25 11:40:20 +0900

    feat: querydsl

--- src/main/java/com/example/books/repository/PostRepository.java
+++ src/main/java/com/example/books/repository/PostRepository.java
@@ -2,8 +2,15 @@ package com.example.books.repository;
 
 import com.example.books.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostQuerydslRepository {
+    List<Post> findAllById(Long userId);
+
+    boolean findByTitle(String title);
+
+    void deleteByTitle(String title);
 }
